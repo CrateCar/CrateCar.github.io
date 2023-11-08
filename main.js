e9e0c7c4399312e8768657414a9ed43c61dfb0bf
yooooj <69322096+CrateCar@users.noreply.github.com>
2023-11-08 08:15:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 window.onload =() => {
     document.getElementById("hero-display").style.transform = "translate(0px, 0px)";
     document.getElementById("hero-display").style.opacity = "1";
+    setTimeout(()=>{
+        document.getElementById("hero-display").style.transition = "none";  
+    },500);
 };
